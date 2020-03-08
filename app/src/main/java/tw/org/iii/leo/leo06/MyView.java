98bebddd5b154fef65fbceeafbcd205fc914e6df
Leo <icpc0928@gmail.com>
2020-03-08 15:33:33 +0800

    03081400 signature app 多線畫圖

--- app/src/main/java/tw/org/iii/leo/leo06/MyView.java
+++ app/src/main/java/tw/org/iii/leo/leo06/MyView.java
@@ -15,13 +15,18 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class MyView extends View {
-    private LinkedList<HashMap<String,Float>> line;
+    private Paint paint ;
+
+    private LinkedList<LinkedList<HashMap<String,Float>>>  lines;
 
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         //setBackgroundColor(Color.GREEN);
 
-        line = new LinkedList<>();
+        lines = new LinkedList<>();
+        paint = new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(10);
 
     }
 
@@ -30,31 +35,48 @@ public class MyView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-//        Log.v("leo","onDraw()");
-        Paint paint = new Paint();
-        paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(10);
-//        canvas.drawLine(0,0,200,200,paint);
-        //十個點有九條線
-        for(int i =1 ; i<line.size(); i++){
-            HashMap<String,Float> p0 = line.get(i-1);
-            HashMap<String,Float> p1 = line.get(i);
-            canvas.drawLine(p0.get("x"),p0.get("y"),p1.get("x"),p1.get("y"),paint);
 
+        //forEach
+        for(LinkedList<HashMap<String,Float>> line : lines){
+            //十個點有九條線
+            for(int i =1 ; i<line.size(); i++){
+                HashMap<String,Float> p0 = line.get(i-1);
+                HashMap<String,Float> p1 = line.get(i);
+                canvas.drawLine(p0.get("x"),p0.get("y"),p1.get("x"),p1.get("y"),paint);
+
+            }
         }
+
+
+
 
 
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+
+        //螢幕點下去我做一條新的線
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+//            Log.v("leo","down");
+            LinkedList<HashMap<String,Float>> line = new LinkedList<>(); //新的線
+            //將點放入線中
+            lines.add(line);
+        }
+
+
         float ex = event.getX(), ey= event.getY();
         Log.v("leo",ex+" x "+ey);
         //點的結構
         HashMap<String,Float> point = new HashMap<>();
         point.put("x",ex); point.put("y",ey);
-        //將點放入線中
-        line.add(point);
+
+        lines.getLast().add(point);
+
+
+
+
+
 
         invalidate();    //讓ondraw方法再度被呼叫（相當於java的 repaint()方法）
         return true; //return super.onTouchEvent(event); return true ; return false; 有三種招,第一種還會叫到onclick跟onlongclick
