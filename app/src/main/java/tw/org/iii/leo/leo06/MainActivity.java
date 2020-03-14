bdd6c65afdda4be39f5a8ed5faca70c1f0f6b176
Leo <icpc0928@gmail.com>
2020-03-14 09:32:20 +0800

    03081400 signature app 多線畫圖 + 清除畫面

--- app/src/main/java/tw/org/iii/leo/leo06/MainActivity.java
+++ app/src/main/java/tw/org/iii/leo/leo06/MainActivity.java
@@ -2,8 +2,15 @@ package tw.org.iii.leo.leo06;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 
 public class MainActivity extends AppCompatActivity {
     private MyView myView;
@@ -17,5 +24,39 @@ public class MainActivity extends AppCompatActivity {
 
     public void clearMyView(View view) {
         myView.clear();
+    }
+
+    public void undoMyView(View view) {
+        myView.undo();
+    }
+    public void redoMyView(View view) {
+        myView.redo();
+    }
+
+    public void chColor(View view) {
+        ColorPickerDialogBuilder
+                .with(this)
+                .setTitle("Choose color")
+                .initialColor(myView.getColor())
+                .wheelType(ColorPickerView.WHEEL_TYPE.CIRCLE)
+                .density(12)
+                .setOnColorSelectedListener(new OnColorSelectedListener() {
+                    @Override
+                    public void onColorSelected(int selectedColor) {
+                        //toast("onColorSelected: 0x" + Integer.toHexString(selectedColor));
+                    }
+                })
+                .setPositiveButton("ok", new ColorPickerClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                        //changeBackgroundColor(selectedColor);
+                        //真正選到顏色在這邊
+                        myView.setColor(selectedColor);
+                    }
+                })
+                .setNegativeButton("cancel", null)
+                .build()
+                .show();
+
     }
 }
--- app/src/main/java/tw/org/iii/leo/leo06/MyView.java
+++ app/src/main/java/tw/org/iii/leo/leo06/MyView.java
@@ -16,15 +16,17 @@ import java.util.LinkedList;
 
 public class MyView extends View {
     private Paint paint ;
-    private LinkedList<LinkedList<HashMap<String,Float>>>  lines;
+    private LinkedList<LinkedList<HashMap<String,Float>>>  lines , recycler;
+    private int color = Color.BLUE ;
 
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         //setBackgroundColor(Color.GREEN);
 
         lines = new LinkedList<>();
+        recycler = new LinkedList<>();
         paint = new Paint();
-        paint.setColor(Color.BLUE);
+        paint.setColor(color);
         paint.setStrokeWidth(10);
 
     }
@@ -37,8 +39,13 @@ public class MyView extends View {
 
         //forEach
         for(LinkedList<HashMap<String,Float>> line : lines){
+            //目前有添加lines的顏色資訊 所以在尋訪時額外取出來
+            HashMap<String,Float> color = line.get(0);
+            //Float 如何轉為int?
+            paint.setColor(color.get("color").intValue());
             //十個點有九條線
-            for(int i =1 ; i<line.size(); i++){
+            //因為第一個是顏色資訊了所以從第二個開始
+            for(int i =2 ; i<line.size(); i++){
                 HashMap<String,Float> p0 = line.get(i-1);
                 HashMap<String,Float> p1 = line.get(i);
                 canvas.drawLine(p0.get("x"),p0.get("y"),p1.get("x"),p1.get("y"),paint);
@@ -46,10 +53,16 @@ public class MyView extends View {
             }
         }
 
+    }
 
+    //顏色對外提供
+    public void setColor(int newColor){
+        color = newColor;
+        invalidate();
+    }
 
-
-
+    public int getColor(){
+        return color;
     }
 
     @Override
@@ -57,25 +70,25 @@ public class MyView extends View {
 
         //螢幕點下去我做一條新的線
         if (event.getAction() == MotionEvent.ACTION_DOWN){
+            recycler.clear();
 //            Log.v("leo","down");
             LinkedList<HashMap<String,Float>> line = new LinkedList<>(); //新的線
+            //將lines的第一個資料為顏色資料
+            HashMap<String,Float> setting = new HashMap<>();
+            setting.put("color",(float)color);   //int 丟到 float ＝>ok; int -> Integer x-> Float 。做轉型 int-> float -> Float
+            line.add(setting);
             //將點放入線中
             lines.add(line);
         }
 
 
         float ex = event.getX(), ey= event.getY();
-        Log.v("leo",ex+" x "+ey);
+//        Log.v("leo",ex+" x "+ey);
         //點的結構
         HashMap<String,Float> point = new HashMap<>();
         point.put("x",ex); point.put("y",ey);
 
         lines.getLast().add(point);
-
-
-
-
-
 
         invalidate();    //讓ondraw方法再度被呼叫（相當於java的 repaint()方法）
         return true; //return super.onTouchEvent(event); return true ; return false; 有三種招,第一種還會叫到onclick跟onlongclick
@@ -85,5 +98,21 @@ public class MyView extends View {
     public void clear(){
         lines.clear();
         invalidate();
+    }
+
+    public void undo(){
+        if(lines.size()>0){
+            recycler.add(lines.removeLast());
+            invalidate();
+        }
+
+
+    }
+
+    public void redo(){
+        if (recycler.size()>0){
+            lines.add(recycler.removeLast());
+            invalidate();
+        }
     }
 }
